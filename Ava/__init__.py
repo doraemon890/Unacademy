71485743b61070821d951dee6bc2f7ada293a86c
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-07-25 17:58:50 +0530

    Update __init__.py

--- Ava/__init__.py
+++ Ava/__init__.py
@@ -1,20 +1,18 @@
 import asyncio
 import logging
-from pyromod import listen
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
-
-
-loop = asyncio.get_event_loop()
-
+# Configure logging
 logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s",
     level=logging.INFO,
 )
 
+# Initialize the event loop
+loop = asyncio.get_event_loop()
 
-
+# Initialize the Pyrogram client
 app = Client(
     ":Ava:",
     api_id=API_ID,
@@ -22,25 +20,17 @@ app = Client(
     bot_token=BOT_TOKEN,
 )
 
-
-
-
-
-
-
-
 async def info_bot():
     global BOT_ID, BOT_NAME, BOT_USERNAME
+    
+    # Start the client
     await app.start()
+    
+    # Retrieve bot information
     getme = await app.get_me()
     BOT_ID = getme.id
     BOT_USERNAME = getme.username
-    if getme.last_name:
-        BOT_NAME = getme.first_name + " " + getme.last_name
-    else:
-        BOT_NAME = getme.first_name
+    BOT_NAME = f"{getme.first_name} {getme.last_name}" if getme.last_name else getme.first_name
 
-
+# Run the info_bot function until complete
 loop.run_until_complete(info_bot())
-
-
