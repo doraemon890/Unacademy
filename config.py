1946832d4e7b0c3961471b6c303e33b30e334ae6
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-07-26 14:22:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
+from dotenv import load_dotenv
 
-# Retrieve environment variables and convert them to the appropriate types
+# Load environment variables from .env file
+load_dotenv()
+
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID"))
-
-
-# Database connection URL
 MONGO_URL = os.getenv("MONGO_URL")
